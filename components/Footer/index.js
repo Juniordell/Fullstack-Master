8657a4931dc5caedc4fb6ab4f4f5eb3a3d33aa85
Nelson Jr <juniordell123@gmail.com>
2020-07-06 23:28:35 -0300

    Add utils

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,9 +5,9 @@ const Footer = () => {
         <div className="bg-gray-700 p-4">
             <div className="container mx-auto text-center font-bold text-white">
                 Projeto desenvolvido por: {'  '}
-                <a className="px-2 hover:underline" href="https://nelsonjrportfolio.imfast.io/">Nelson Jr</a> / {'  '}
-                <a className="px-2 hover:underline" href="https://www.linkedin.com/in/nelson-dellosbel-junior-ba80211ab/">Linkedin</a> / {'  '} 
-                <a className="px-2 hover:underline" href="https://github.com/Juniordell">Github</a>
+                <a className="px-2 hover:underline" href="https://nelsonjrportfolio.imfast.io/" target="_blank">Nelson Jr</a> / {'  '}
+                <a className="px-2 hover:underline" href="https://www.linkedin.com/in/nelson-dellosbel-junior-ba80211ab/" target="_blank">Linkedin</a> / {'  '} 
+                <a className="px-2 hover:underline" href="https://github.com/Juniordell" target="_blank">Github</a>
                 <div className="mt-4">
                     <img className="inline p-4 w-64" src="/logo_semana_fsm.png" />
                     <img className="inline p-4" src="/logo_devpleno.png" />
